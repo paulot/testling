f3427e9c1218de0e30332e07ddc830fdcce5d108
Paulo Tanaka <ptanaka@linkedin.com>
2016-02-29 14:28:30 -0800

    Adding client library

---
+++ Testling/Testling/Testling.h
@@ -0,0 +1,19 @@
+//
+//  Testling.h
+//  Testling
+//
+//  Created by Paulo Tanaka on 2/29/16.
+//  Copyright © 2016 Paulo Tanaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Testling.
+FOUNDATION_EXPORT double TestlingVersionNumber;
+
+//! Project version string for Testling.
+FOUNDATION_EXPORT const unsigned char TestlingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Testling/PublicHeader.h>
+
+
